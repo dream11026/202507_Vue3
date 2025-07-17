638cdfad81aad122b4a03dc63a304f80e5516da2
dream11026 <dream11026@gmail.com>
2025-07-17 10:34:08 +0800

    ref參照到物件 新增CompositionComponent4 的 vbase3 空殼

--- lab12_vite/src/App.vue
+++ lab12_vite/src/App.vue
@@ -11,18 +11,27 @@
     <CompositionComponent2></CompositionComponent2>
     <!-- kebab  case -->
     <!-- <composition-component2></composition-component2> -->
+
+    <hr />
+    <composition-component3></composition-component3>
+    <hr/>
+    <composition-component4></composition-component4>
   </div>
 </template>
 
 <script>
-import CompositionComponent1 from './components/CompositionComponent1.vue';
-import CompositionComponent2 from './components/CompositionComponent2.vue';
+import CompositionComponent1 from './components/CompositionComponent1.vue'
+import CompositionComponent2 from './components/CompositionComponent2.vue'
+import CompositionComponent3 from './components/CompositionComponent3.vue'
+import CompositionComponent4 from './components/CompositionComponent4.vue'
 
 export default {
   components: {
     //CompositionComponent1,
     "composition-component1": CompositionComponent1, // 指定只有一種命名
-    CompositionComponent2 // 內建支援兩種 命名方式
+    CompositionComponent2, // 內建支援兩種 命名方式
+    CompositionComponent3,
+    CompositionComponent4
   },
   data() {
     return {
--- lab12_vite/src/components/CompositionComponent3.vue
+++ lab12_vite/src/components/CompositionComponent3.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
-
+        <h1>ref參照到物件</h1>
+        <p>{{ user }}</p>
+        <p>userName:{{ user.userName }},age:{{ user.age }}</p>
+        <h1>如果不是reactive的就不會變</h1>
+        <p>{{ pUserName }},{{ pAge }}</p>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
     setup() {
+        const user = ref({
+            userName: "dream",
+            age: 49
+        })
+        setTimeout(() => {
+            user.value.userName = "一段時間更新 ref"
+            user.value.age += 1
+        }, 2000)
+        return {
+            user: user,
 
-
-        return {}
+            // 以下直接取值，非reactive元件
+            pUserName: user.value.userName,
+            pAge: user.value.age
+        }
     }
 }
 </script>
