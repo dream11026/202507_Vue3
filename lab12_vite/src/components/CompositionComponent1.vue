a7f6a0cbe86d60180f15753afcaa3282ffa0d858
dream11026 <dream11026@gmail.com>
2025-07-17 10:17:15 +0800

    練習變數與ref變數更新情境 新增CompositionComponent3 的 vbase3 空殼

--- lab12_vite/src/components/CompositionComponent1.vue
+++ lab12_vite/src/components/CompositionComponent1.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
-        <h1>composition component1</h1>
+        <h1>Composition Component1</h1>
+        <h3>userName:{{ userName }}</h3>
+        <p>age:{{ age }}</p>
+        <h3>userName2:{{ userName2 }}</h3>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
     setup() {
 
+        let userName = "dream"
+        let userName2 = ref("dream")
+        let currentAge = ref(49)
+        console.log("userName type:", typeof userName, userName)
+        console.log("userName2 type:", typeof userName2, userName2)
 
-        return {}
+        setTimeout(() => {
+            userName = "一段時間更新"
+            userName2.value = "一段時間更新 ref"
+            currentAge.value += 1
+
+            console.log("userName type:", typeof userName, userName)
+            console.log("userName2 type:", typeof userName2, userName2)
+        }, 2000)
+        return { userName, userName2, age: currentAge }
     }
 }
 </script>
--- lab12_vite/src/components/CompositionComponent2.vue
+++ lab12_vite/src/components/CompositionComponent2.vue
@@ -1,19 +1,32 @@
 <template>
     <div>
-        <h1>Composition Component2</h1>
+        <h1>Composition Component2 in script setup</h1>
+        <h1>Dom會一起刷新</h1>
+        <h3>userName:{{ userName }}</h3>
+
+        <!-- script setup 方式 沒有另外return 更換變數名稱 -->
+        <p>age:{{ currentAge }}</p>
+
+        <h3>userName2:{{ userName2 }}</h3>
     </div>
 </template>
 
-<script>
-export default {
-    setup () {
-        
+<script setup>
+import { ref } from 'vue'
+let userName = "dream"
+let userName2 = ref("dream")
+let currentAge = ref(49)
 
-        return {}
-    }
-}
+console.log("userName type:", typeof userName, userName)
+console.log("userName2 type:", typeof userName2, userName2)
+setTimeout(() => {
+    userName = "一段時間更新" // 因dom刷新，而一起更新，本身不適reactive物件
+    userName2.value = "一段時間更新 ref"
+    currentAge.value += 1
+
+    console.log("userName type:", typeof userName, userName)
+    console.log("userName2 type:", typeof userName2, userName2)
+}, 2000)
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
