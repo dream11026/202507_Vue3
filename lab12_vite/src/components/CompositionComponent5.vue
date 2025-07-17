d7d781f394ca51b090d8e98e95aa28db63b10811
dream11026 <dream11026@gmail.com>
2025-07-17 11:32:23 +0800

    reactive to refs v2

--- lab12_vite/src/components/CompositionComponent5.vue
+++ lab12_vite/src/components/CompositionComponent5.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <h1>5. reactive參照到物件</h1>
+        <h1>5. reactive參照到物件....</h1>
         <p>{{ user }}</p>
-        <p>userName:{{ user.userName }},age:{{ user.age }}</p>
-        <h1>如果不是reactive的就不會變</h1>
-        <p>{{ pUserName }},{{ pAge }}</p>
+        <h1>toRefs的ref參照到物件的屬性</h1>
+        <p>{{ userName }}</p>
+        <p>{{ age }}</p>
     </div>
 </template>
 
@@ -19,22 +19,27 @@ export default {
         })
 
         console.log("reactive user=", user)
-        const userReactiveToRefs = toRefs(user)
-        console.log(" after torefs, ", userReactiveToRefs)
-        console.log("user底下的userName包裝後是ref嗎?", isRef(userReactiveToRefs.userName))
-        console.log("user底下的age包裝後是ref嗎?", isRef(userReactiveToRefs.age))
+        // const userReactiveToRefs = toRefs(user)
+        // console.log(" after torefs, ", userReactiveToRefs)
+        // console.log("user底下的userName包裝後是ref嗎?", isRef(userReactiveToRefs.userName))
+        // console.log("user底下的age包裝後是ref嗎?", isRef(userReactiveToRefs.age))
+        const { userName, age } = toRefs(user)
 
         setTimeout(() => {
             user.userName = "一段時間更新 reactive"
             user.age += 1
+            setTimeout(() => {
+                userName.value = "第二段時間更新 toRefs"
+                age.value += 5
+            }, 2000)
         }, 2000)
 
         return {
             user: user,
 
             // 以下直接取值，reactive元件
-            pUserName: user.userName,
-            pAge: user.age
+            userName: userName,
+            age: age
         }
     }
 }
