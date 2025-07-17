5a8ad3d360f77d680edba00db076d76a20d45c86
dream11026 <dream11026@gmail.com>
2025-07-17 11:58:51 +0800

    add MyComp1

---
+++ lab13_vite_composition2/src/components/MyComp1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>元件1</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
