57610119b02ee29215b45e1be89e438263f4114c
dream11026 <dream11026@gmail.com>
2025-07-17 14:59:38 +0800

    life cycle hook between 「Options  API」 and 「Composition API」

--- lab13_vite_composition2/src/components/MyComposition2.vue
+++ lab13_vite_composition2/src/components/MyComposition2.vue
@@ -5,9 +5,19 @@
 </template>
 
 <script>
+import { onMounted, onBeforeUnmount } from 'vue';
+
 export default {
     setup() {
+        console.log("MyComposition2 setup 元件初始化")
 
+        onMounted(() => {
+            console.log("MyComposition2 onMounted api掛載之後")
+        })
+
+        onBeforeUnmount(() => {
+            console.log("MyComposition2 onBeforeUnmount 即將移除元件")
+        })
 
         return {}
     }
--- lab13_vite_composition2/src/components/MyOptions2.vue
+++ lab13_vite_composition2/src/components/MyOptions2.vue
@@ -6,7 +6,15 @@
 
 <script>
 export default {
-
+    created() {
+        console.log("MyOptions2 created 也幾乎是初始化之後")
+    },
+    mounted() {
+        console.log("MyOptions2 mounted 掛載元件之後")
+    },
+    beforeUnmount() {
+        console.log("MyOptions2 beforeUnmount 即將移除元件")
+    }
 }
 </script>
 
