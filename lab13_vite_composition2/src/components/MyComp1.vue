cba4a71154eef0303cd80adc3436872012f1f4a4
dream11026 <dream11026@gmail.com>
2025-07-17 12:00:21 +0800

    建立點擊事件 inline 和 function方式

--- lab13_vite_composition2/src/components/MyComp1.vue
+++ lab13_vite_composition2/src/components/MyComp1.vue
@@ -1,15 +1,23 @@
 <template>
     <div>
         <h1>元件1</h1>
+        <h3>{{ course }} 價格是 {{ price }}</h3>
+        <button @click="course.duration = course.duration + 7">加一天課</button>
+        <button @click="extraDuration">用函數加一天課</button>
     </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
 export default {
     setup() {
-
-
-        return {}
+        const course = reactive({ name: "POOP", duration: 35 })
+        const price = ref(24000)
+        function extraDuration() {
+            course.duration = course.duration + 7
+            price.value += 5000
+        }
+        return { course, extraDuration, price }
     }
 }
 </script>
