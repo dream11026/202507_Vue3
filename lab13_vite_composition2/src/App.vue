a20a2be129f3b157ce03e4eea62a318474bf3822
dream11026 <dream11026@gmail.com>
2025-07-17 14:49:30 +0800

    add MyOptions2, MyComposition2 空殼

--- lab13_vite_composition2/src/App.vue
+++ lab13_vite_composition2/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import MyComp1 from './components/MyComp1.vue';
+import MyComposition2 from './components/MyComposition2.vue'
+import MyOptions2 from './components/MyOptions2.vue'
+
 </script>
 
 <template>
   <h1>Composition Lab2</h1>
   <br />
   <MyComp1></MyComp1>
+  <br/>
+  <MyOptions2></MyOptions2>
+  <br/>
+  <MyComposition2></MyComposition2>
 </template>
 
 <style scoped></style>
