36a349fcdf50c25eb270a115fb46e55e4f221b03
dream11026 <dream11026@gmail.com>
2025-07-17 13:41:32 +0800

    use v-model, add watch price

--- lab13_vite_composition2/src/components/MyComp1.vue
+++ lab13_vite_composition2/src/components/MyComp1.vue
@@ -6,13 +6,15 @@
         <button @click="course.duration = course.duration + 7">加一天課</button>
         <button @click="extraDuration">用函數加一天課</button>
         <hr />
-        <input placeholder="courseId" @input="setCourseId" />
-        <input placeholder="courseFullName" @input="setCourseFullName" />
+        <!-- <input placeholder="courseId" @input="setCourseId" />
+        <input placeholder="courseFullName" @input="setCourseFullName" /> -->
+        <input placeholder="courseId" v-model="courseId" />
+        <input placeholder="courseFullName" v-model="courseFullName" />
     </div>
 </template>
 
 <script>
-import { reactive, ref, computed } from 'vue'
+import { reactive, ref, computed, watch } from 'vue'
 export default {
     setup() {
         const course = reactive({ name: "POOP", duration: 35 })
@@ -25,24 +27,29 @@ export default {
         const courseId = ref("")
         const courseFullName = ref("")
 
-        function setCourseId(e) {
-            courseId.value = e.target.value
-        }
+        // function setCourseId(e) {
+        //     courseId.value = e.target.value
+        // }
 
-        function setCourseFullName(e) {
-            courseFullName.value = e.target.value
-        }
+        // function setCourseFullName(e) {
+        //     courseFullName.value = e.target.value
+        // }
 
         // HOF(High Order Function) 使用 computed 轉換函數
         const courseDisplayName = computed(function () {
             return courseId.value + '../..' + courseFullName.value
         })
-        
+
+        watch(price, function (newValue, oldValue) {
+            console.log(`price goes from ${oldValue} to ${newValue}`)
+        })
+
         return {
             course, extraDuration, price,
             courseDisplayName,
-            setCourseId,
-            setCourseFullName
+            // setCourseId,
+            // setCourseFullName,
+            courseId, courseFullName
         }
     }
 }
