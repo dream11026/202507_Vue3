8c1e32f93633fb5cecc1f0cd1d388a7de513c21f
dream11026 <dream11026@gmail.com>
2025-07-17 09:47:32 +0800

    使用components與 練習components 宣告不同命名方式

--- lab12_vite/src/App.vue
+++ lab12_vite/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
+    <h1>composition component practices</h1>
+    <h3>{{ userName }}</h3>
 
+    <!-- 指定只有一種命名 -->
+    <composition-component1></composition-component1>
+    <hr />
+
+    <!-- camel case -->
+    <CompositionComponent2></CompositionComponent2>
+    <!-- kebab  case -->
+    <!-- <composition-component2></composition-component2> -->
   </div>
 </template>
 
 <script>
-  export default {
-    
+import CompositionComponent1 from './components/CompositionComponent1.vue';
+import CompositionComponent2 from './components/CompositionComponent2.vue';
+
+export default {
+  components: {
+    //CompositionComponent1,
+    "composition-component1": CompositionComponent1, // 指定只有一種命名
+    CompositionComponent2 // 內建支援兩種 命名方式
+  },
+  data() {
+    return {
+      userName: "dream"
+    }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ lab12_vite/src/components/CompositionComponent2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Composition Component2</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
